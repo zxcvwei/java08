a1973763f8b2c573a610512c9a4948ce16a87777
zxcvwei <104021031@live.asia.edu.tw>
2015-08-14 10:30:04 +0800

    java08
    array2D

--- array2D_03.java
+++ array2D_03.java
@@ -9,22 +9,40 @@ public class array2D_03 {
 		int p = scn.nextInt();
 		System.out.println("請輸入B為pxn的矩陣");
 		int n = scn.nextInt();
-		
 		int a[][] = new int[m][p];
 		int b[][] = new int[p][n];
 		int c[][] = new int[m][n];
-		for (int i = 0; i <= m; i++) {
-			for (int l = 0; l <= n; l++) {
-				for(int g = 0;g<p;g++){
-					c[m][n] = c[m][n]+a[m][g]*b[g][n];
+		int i = 0;
+		int g = 0;
+		int l = 0;
+		System.out.println("請輸入A矩陣的數字");
+		for (i = 0; i < m; i++) {
+			for (g = 0; g < p; g++) {
+				a[i][g] = scn.nextInt();// [a11] m=2
+			} 							// [a21] p=2
+		} 								// [a12]
+										// [a22]
+		System.out.println("請輸入B矩陣的數字");
+		for (l = 0; l < n; l++) {
+			for (g = 0; g < p; g++) {
+				b[g][l] = scn.nextInt();
+				// [b11] n=3
+				// [b21]
+				// [b12]
+				// [b22]
+				// [b13]
+				// [b23]
+			}
+		}
+		for (i = 0; i < m; i++) {
+			for (l = 0; l < n; l++) {
+				for (g = 0; g < p; g++) {
+					c[g][l] = c[g][l] + a[i][g] * b[g][l];
 				}
-				System.out.print(c[m][n]+"\t");
-				
-				// a[i][j]*b[k][l];
+				System.out.print(c[i][l] + "\t");
 			}
 			System.out.println();
 		}
-
 	}
 
 }
